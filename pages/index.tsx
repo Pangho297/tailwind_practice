2a7c78bba579d9723343a9f798fbf77b01d15d4c
Pangho297 <monanazzang@gmail.com>
2022-05-03 16:40:44 +0900

    First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Start from '../views/Start';
+
+const Home: NextPage = () => {
+  return <Start />;
+};
+
+export default Home;
---
+++ views/Start/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Start: NextPage = () => {
+  return <div className="bg-red-600 w-full text-orange-400">Hello World!</div>;
+};
+
+export default Start;
